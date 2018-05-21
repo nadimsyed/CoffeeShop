be4b1db7c310e4e7f696cde311a3a1ad9d43c9f7
Nadim <syed.m.nadim@gmail.com>
2018-05-21 16:17:21 -0400

    Created UserReg controller, allow user to input information on first registation page, adjusted _Layout so navigation bar shows new option, added a page for after user has succesfully registered.

---
+++ CoffeeShop/CoffeeShop/Controllers/UserRegController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class UserRegController : Controller
+    {
+        // GET: UserReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserReg(string firstName, string lastName, string email, int phoneNumber, string password)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.Email = email;
+            ViewBag.PhoneNumber = phoneNumber;
+            ViewBag.Password = password;
+
+            return View();
+        }
+    }
+}
